49421c8762396bbdf1db44cf569782027a3a56e3
louis3546 <lourensvdbijl@gmail.com>
2021-06-06 17:31:01 +0200

    updaTE

---
+++ vue-basic/src/components/Alert.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+      <div class="alert" :class="mode" role="alert">
+          <slot></slot>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Alert",
+  props: [
+      'mode'
+  ]
+ 
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+.primary {
+    color: #fff;
+    background-color: #cce5ff;
+    border-color: #b8daff;
+}
+.success {
+    color: #fff;
+    background-color: #07b61060;
+    border-color: #02610f;
+}
+.danger {
+    color: #fff;
+    background-color: #fa01018f;
+    border-color: #aa0202;
+}
+.alert {
+    position: relative;
+    padding: .75rem 1.25rem;
+    margin-bottom: 1rem;
+    border: 1px solid transparent;
+    border-radius: .25rem;
+}
+</style>
---
+++ vue-basic/src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <alert mode="danger">First</alert>
+    <alert mode="primary">Second</alert>
+    <alert mode="success">Third</alert>
+
+  </div>
+</template>
+
+<script>
+import Alert from './Alert.vue';
+export default {
+  components: { Alert },
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
